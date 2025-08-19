2d328985da795045748b70d7974a75a4661452db
ahammed-protiva <protivaahammed7137@gmail.com>
2025-08-19 15:09:53 +0900

    binary-search-in-rotated-array

---
+++ cpProblems/binary_search_rotated_sorted_array.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution10:
+    def search(self, nums: List[int], target: int) -> int:
+        l ,r = 0 ,len(nums) -1 
+        while  l <= r :
+            m = (l + r)//2
+            if nums[m] == target:
+                return m
+
+            #left sorted segment
+            if nums[l] <= nums[m]:
+                if target > nums[m] or target < nums[l] :
+                    l = m + 1 
+                else :
+                    r = m - 1
+            #right sorted segment
+            else :
+                if target < nums[m] or target > nums[r]:
+                    r = m - 1 
+                else :
+                    l = m + 1
+        return -1
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from cpProblems.Binary_search import Solution8
+from cpProblems.binary_search import Solution8
 from cpProblems.prefix_sufix_sum import Solution1
 from cpProblems.longest_consecutive_consequnce import Solution2
 from cpProblems.search_2d_matrix import Solution9
@@ -6,7 +6,7 @@ from cpProblems.trap_rain_water import Solution7
 from cpProblems.two_sum import Solution3
 from cpProblems.three_sum_two_pointer import Solution5
 from cpProblems.maximum_water_container import Solution6
-
+from cpProblems.binary_search_rotated_sorted_array import Solution10
 def main():
       # Take input from user
     input_str = input("Enter numbers separated by spaces: ")
@@ -15,13 +15,13 @@ def main():
     nums = list(map(int, input_str.strip().split()))
 
     #2D matrix input taking
-    matrix = []
-    rows = int(input("row : "))
-    cols = int (input("cols : "))
-    for i in range (rows):
-      row_input = input().split()
-      row = [int(x) for x in row_input]
-      matrix.append(row)
+    # matrix = []
+    # rows = int(input("row : "))
+    # cols = int (input("cols : "))
+    # for i in range (rows):
+    #   row_input = input().split()
+    #   row = [int(x) for x in row_input]
+    #   matrix.append(row)
     
 
 
@@ -30,12 +30,13 @@ def main():
 
     
     print("Running problem :")
-    solve = Solution9()
+    solve = Solution10()
     # res = solve.longestConsecutive(nums)
     # res = solve.maxAreaTwoPointer(nums)
-    res = solve.searchMatrixBrute(matrix, 5)
-    res1 = solve.searchMatrixBinary(matrix , 3)
-    print(res)
+    # res = solve.searchMatrixBrute(matrix, 5)
+    # res1 = solve.searchMatrixBinary(matrix , 3)
+    ans = solve.search(nums , 0)
+    print(ans)
 
 
 
